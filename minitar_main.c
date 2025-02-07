28e96f33f05b4446cbe3367871d1fb251937f6c8
root <khan0838@umn.edu>
2025-02-07 22:14:15 +0000

    added files

---
+++ minitar_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "file_list.h"
+#include "minitar.h"
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        printf("Usage: %s -c|a|t|u|x -f ARCHIVE [FILE...]\n", argv[0]);
+        return 0;
+    }
+
+    file_list_t files;
+    file_list_init(&files);
+
+    // TODO: Parse command-line arguments and invoke functions from 'minitar.h'
+    // to execute archive operations
+
+    file_list_clear(&files);
+    return 0;
+}
