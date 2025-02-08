a5275240d92a3884c1c6960ae0ee01a76a6b2e4b
root <khan0838@umn.edu>
2025-02-08 00:48:38 +0000

    initialized all the linked list

--- minitar_main.c
+++ minitar_main.c
@@ -9,13 +9,53 @@ int main(int argc, char **argv) {
         printf("Usage: %s -c|a|t|u|x -f ARCHIVE [FILE...]\n", argv[0]);
         return 0;
     }
+    else{
+        file_list_t files;
+        file_list_init(&files);
+        char *archive_name = NULL;
 
-    file_list_t files;
-    file_list_init(&files);
+        //checking if the user is using the -f command to add an archive file
+        if(strcmp(argv[2], "-f") == 0){
+            archive_name = argv[3];
+        }
+        else{
+            printf("Error: no -f input for archive name");
+            file_list_clear(&files);
+            return 0;
+        }
+        int i = 4;
+        files.size ++;
+        //initializing the linked list of files
+        while(i < argc){
+            file_list_add(&files, argv[i]);
+            i++;
+        }
 
-    // TODO: Parse command-line arguments and invoke functions from 'minitar.h'
-    // to execute archive operations
+        if(strcmp(argv[1], "-c") == 0){
 
-    file_list_clear(&files);
-    return 0;
+
+        }
+        else if(strcmp(argv[1], "-a") == 0){
+
+        }
+        else if(strcmp(argv[1], "-t") == 0){
+
+        }
+        else if(strcmp(argv[1], "-u") == 0){
+
+        }
+        else if(strcmp(argv[1], "-x") == 0){
+
+        }
+        else{
+            printf("Error: no operation given");
+            file_list_clear(&files);
+            return 0;
+        }
+
+
+
+        file_list_clear(&files);
+        return 0;
+    }
 }
