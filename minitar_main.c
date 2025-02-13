d01dd2b8ed455ed9f0f19a716cd9ea58e4b07afe
Daniyal Khan <khan0838@umn.edu>
2025-02-13 20:44:24 +0000

    made final version of get and append and main

--- minitar_main.c
+++ minitar_main.c
@@ -13,47 +13,113 @@ int main(int argc, char **argv) {
     file_list_init(&files);
     char *archive_name = NULL;
 
-        // checking if the user is using the -f command to add an archive file
+    // checking if the user is using the -f command to add an archive file
     if (strcmp(argv[2], "-f") == 0) {
         archive_name = argv[3];
-    }
-    else {
+    } else {
         printf("Error: no -f input for archive name\n");
         file_list_clear(&files);
-        return 0;
+        return 1;
     }
-    // initializing the linked list of files
 
-    for(int i = 4; i<argc; i++){
+    // initializing the linked list of files
+    for (int i = 4; i < argc; i++) {
         file_list_add(&files, argv[i]);
     }
 
-
-    //switch case for each of the command options
+    // switch case for each of the command options
     if (strcmp(argv[1], "-c") == 0) {
-        create_archive(archive_name, &files);
-
-    }
-    else if (strcmp(argv[1], "-a") == 0) {
-            append_files_to_archive(archive_name, &files);
+        if (create_archive(archive_name, &files) == 0) {
+            file_list_clear(&files);
+            return 0;
+        } else {
+            perror("Error: something went with creating archive\n");
+            file_list_clear(&files);
+            return 1;
         }
 
+    } else if (strcmp(argv[1], "-a") == 0) {
+        if (append_files_to_archive(archive_name, &files) == 0) {
+            file_list_clear(&files);
+            return 0;
+        } else {
+            printf("Error: something went with appending archive %s\n", archive_name);
+            file_list_clear(&files);
+            return 1;
+        }
+    }
+
     else if (strcmp(argv[1], "-t") == 0) {
-        get_archive_file_list(archive_name, &files);
+        if (get_archive_file_list(archive_name, &files) == 0) {
+            // traverses the linked list and prints out the name of the files
+            node_t *current = files.head;
+            while (current != NULL) {
+                printf("%s\n", current->name);
+                current = current->next;
+            }
+            file_list_clear(&files);
+            return 0;
+        } else {
+            perror("Error: something went with getting archive\n");
+            file_list_clear(&files);
+            return 1;
+        }
     }
 
     else if (strcmp(argv[1], "-u") == 0) {
-            update_files_to_archive(archive_name, &files);
+        file_list_t oldFiles;
+        file_list_init(&oldFiles);
+
+        //access old files
+        if(get_archive_file_list(archive_name, &oldFiles) == -1){
+            perror("Failed to call get method");
+            file_list_clear(&files);
+            file_list_clear(&oldFiles);
+            return 1;
         }
 
-    else if (strcmp(argv[1], "-x") == 0) {
-            extract_files_from_archive(archive_name);
+        if(file_list_is_subset(&files, &oldFiles) == 0){
+            printf("Error: One or more of the specified files is not already present in archive");
+            file_list_clear(&files);
+            file_list_clear(&oldFiles);
+
+            return 1;
         }
+        else{
+            if(append_files_to_archive(archive_name, &files) == -1){
+                perror("could not append file");
+                file_list_clear(&files);
+                file_list_clear(&oldFiles);
+                return 1;
+
+            }
+            file_list_clear(&files);
+            file_list_clear(&oldFiles);
+            return 0;
+
+        }
+        file_list_clear(&files);
+        file_list_clear(&oldFiles);
+        return 0;
+
+
+
+    }
+
+    else if (strcmp(argv[1], "-x") == 0) {
+        if (extract_files_from_archive(archive_name) == 0) {
+            file_list_clear(&files);
+            return 0;
+        } else {
+            perror("Error: something went with extracting archive\n");
+            file_list_clear(&files);
+            return 1;
+        }
+    }
+
     else {
         printf("Error: no operation given\n");
         file_list_clear(&files);
-        return 0;
+        return 1;
     }
-    file_list_clear(&files);
-    return 0;
 }
